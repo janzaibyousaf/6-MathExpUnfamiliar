8d057c63dc2f4119621ee8343c0eece2eee4f42c
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-03-12 22:44:53 +0500

    Add files via upload

---
+++ 6-MathExpUnfamiliar/app.js
@@ -0,0 +1,20 @@
+var num = 10;
+
+num = num + 1
+
+console.log(num)
+
+var num1 = 10;
+
+num++;
+console.log(num)
+
+var num2 = 10;
+
+num--;
+console.log(num)
+
+var num = 10;
+
+console.log(num++)
+console.log(num)
